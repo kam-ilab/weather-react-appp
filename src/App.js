789040b067206aefeaccf8f2dff63b6b18c8120d
kam-ilab <146891307+kam-ilab@users.noreply.github.com>
2024-05-02 17:57:08 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import axios from "axios";
 
 function App() {
   np;
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Kamila Baas and is{" "}
+        <a
+          href="https://github.com/kam-ilab/weather-react-appp"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
